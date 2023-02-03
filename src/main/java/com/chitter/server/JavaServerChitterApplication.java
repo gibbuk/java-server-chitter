a0aa037b90f3df02f42fd56e89bf10e180332cac
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-03 10:27:25 +0000

    Initial project setup

---
+++ src/main/java/com/chitter/server/JavaServerChitterApplication.java
@@ -0,0 +1,13 @@
+package com.chitter.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServerChitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerChitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chitter/server/JavaServerChitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitter.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerChitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
