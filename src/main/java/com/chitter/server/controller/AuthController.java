89fe0a6cd5285384f62f462de2cd762ef5b1cff3
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-06 14:58:48 +0000

    Add AuthController and suporting classes

---
+++ src/main/java/com/chitter/server/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.chitter.server.controller;
+
+
+import com.chitter.server.model.User;
+import com.chitter.server.payload.response.MessageResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chitter.server.repository.UserRepository;
+
+
+@RestController
+@RequestMapping
+public class AuthController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<MessageResponse> registerUser(@RequestBody User user) {
+
+        try {
+
+            userRepository.save(new User(user.getUsername(), user.getName(), user.getEmail(), user.getPassword()));
+
+            MessageResponse message = new MessageResponse("Sign up successful");
+
+            return new ResponseEntity<>(message, HttpStatus.CREATED);
+
+        } catch(Exception e){
+            return new ResponseEntity<>(new MessageResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ src/test/java/com/chitter/server/AuthControllerTests.java
@@ -0,0 +1,62 @@
+package com.chitter.server;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.chitter.server.controller.AuthController;
+import com.chitter.server.model.User;
+import com.chitter.server.repository.UserRepository;
+
+
+@WebMvcTest(AuthController.class)
+public class AuthControllerTests {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    void shouldReturnASuccessMessagePayloadOnReceivingValidRequest() throws Exception {
+        User user = new User("username", "name", "email@email.com", "password");
+
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Sign up successful"))
+                .andDo(print());
+    }
+
+    @Test
+    void shouldReturnInternalServerErrorInEventOfException() throws Exception {
+        User user = new User("username", "name", "email@email.com", "password");
+        RuntimeException e = new RuntimeException("Server Message");
+
+        when(userRepository.save(Mockito.any(User.class))).thenThrow(e);
+        mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value(e.getMessage()))
+                .andDo(print());
+    }
+
+
+
+
+}
